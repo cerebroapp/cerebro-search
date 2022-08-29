b66f3b38b8402c45dd4f6d03611f4122310c559a
dubisdev <dubisdev@gmail.com>
2022-08-29 16:26:15 +0200

    chore: check internet connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export { default as settings } from "./settings";
 
 export const fn = ({ term, actions, settings, display }) => {
+  if (!navigator.onLine) return;
+
   if (settings["Search Link"]) {
     const title = `Search ${term}`;
     const onSelect = () => actions.open(settings["Search Link"] + term);
