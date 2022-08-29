126fcd2456735754d0acd78bc96a29d73d719de5
dubisdev <dubisdev@gmail.com>
2022-08-29 18:43:40 +0200

    chore: start working in google engine

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
-export { default as settings } from "./settings";
+import { google } from "handlers";
+import { Engine } from "types";
 import icon from "./icons/default.png";
 
 export const fn = ({ term, actions, settings, display }) => {
   if (!navigator.onLine) return;
 
+  // handle custom search engine
   if (settings["Search Link"]) {
     const title = `Search ${term}`;
     const onSelect = () => actions.open(settings["Search Link"] + term);
     return display({ title, onSelect, icon });
   }
+
+  // handle built-in search engines
+  switch (settings["Search Engine"] as Engine) {
+    case "Google":
+      return google({ term, actions, display });
+  }
 };
+
+export { default as settings } from "./settings";
