ad5a869d4ccacf35d7058c3a0291575820ebc4c6
dubisdev <dubisdev@gmail.com>
2022-09-02 00:12:20 +0200

    fix: queryParams infinite assignments

--- src/handlers/google/index.tsx
+++ src/handlers/google/index.tsx
@@ -2,9 +2,9 @@ import { EngineHandler, Suggestion } from "types";
 import icon from "icons/google.png";
 import ShowSuggestions from "components/ShowSuggestions";
 
-const SUGGESTIONS_URL = new URL(
-  "http://suggestqueries.google.com/complete/search?client=firefox"
-);
+const SUGGESTIONS_URL =
+  "http://suggestqueries.google.com/complete/search?client=firefox&q=";
+
 const SEARCH_URL = "https://www.google.com/search?q=";
 
 const google: EngineHandler = ({ term, actions, display }) => {
@@ -35,9 +35,7 @@ export default google;
  * Get google suggestions for entered query
  */
 const getSuggestions = async (term: string) => {
-  SUGGESTIONS_URL.searchParams.append("q", term);
-
-  return fetch(SUGGESTIONS_URL)
+  return fetch(`${SUGGESTIONS_URL}${term}`)
     .then((response) => response.json())
     .then((response) => (response[1] || []) as string[])
     .then((res) => res.map(mapToSuggestion));
