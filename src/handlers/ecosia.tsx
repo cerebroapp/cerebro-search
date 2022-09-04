6cabfdbc78e8bf8c95fce7807a45e25f4f41e12f
dubisdev <dubisdev@gmail.com>
2022-09-04 13:15:41 +0200

    feat: add ecosia search

---
+++ src/handlers/ecosia.tsx
@@ -0,0 +1,45 @@
+import { EngineHandler, Suggestion, SuggestionGetter } from "types";
+import icon from "icons/ecosia.png";
+import ShowSuggestions from "components/ShowSuggestions";
+
+const SUGGESTIONS_URL = "http://ac.ecosia.org/autocomplete?q=";
+
+const SEARCH_URL = "https://www.ecosia.org/search?q=";
+
+const ecosia: EngineHandler = ({ term, actions, display }) => {
+  const title = `Search: ${term}`;
+
+  const searchFn = (q: string) => {
+    actions.open(`${SEARCH_URL}${q}`);
+    actions.hideWindow();
+  };
+
+  display({
+    title,
+    icon,
+    onSelect: () => searchFn(term),
+    getPreview: () => (
+      <ShowSuggestions
+        suggestionGetter={getSuggestions}
+        term={term}
+        searchFn={searchFn}
+      />
+    ),
+  });
+};
+
+export default ecosia;
+
+/**
+ * Get suggestions for entered query
+ */
+const getSuggestions: SuggestionGetter = async (term: string) => {
+  return fetch(`${SUGGESTIONS_URL}${term}`)
+    .then((response) => response.json())
+    .then((response) => (response.suggestions || []) as string[])
+    .then((res) => res.map(mapToSuggestion));
+};
+
+const mapToSuggestion = (suggestion: string): Suggestion => ({
+  title: suggestion,
+});
--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,2 +1,3 @@
 export { default as google } from "./google";
 export { default as duckduckgo } from "./duckduckgo";
+export { default as ecosia } from "./ecosia";
